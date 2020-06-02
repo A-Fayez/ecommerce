643453b8c64c59c53d3f6b5e372ebd8e3e242fb2
a-fayez <ahmedf.higgy@gmail.com>
2020-06-02 16:52:15 +0200

    login and registration pages

--- orders/views.py
+++ orders/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "pizza/homepage.html")
+
+
+def login(request):
+    return render(request, "pizza/login.html")
+
+
+def register(request):
+    return render(request, "pizza/register.html")
