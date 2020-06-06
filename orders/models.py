a5bbc24097a43e039e86e520af12e14bf8fddf5c
a-fayez <ahmedf.higgy@gmail.com>
2020-06-06 06:43:11 +0200

    menu modles

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
 # Create your models here.
+class Topping(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.DecimalField(blank=True)
+
+
+# add-ons on the subs
+class Extra(models.Model):
+    name = models.CharField(max_length=64)
+    small_price = models.DecimalField()
+    large_price = models.DecimalField()
+
+
+class RegularPizza(models.Model):
+    name = models.CharField(max_length=64)
+    small_price = models.DecimalField()
+    large_price = models.DecimalField()
+    toppings = models.ManyToManyField(Topping)
+
+
+class SicilianPizza(models.Model):
+    name = models.CharField(max_length=64)
+    small_price = models.DecimalField()
+    large_price = models.DecimalField()
+    toppings = models.ManyToManyField(Topping)
+
+
+class Sub(models.Model):
+    name = models.CharField(max_length=64)
+    small_price = models.DecimalField()
+    large_price = models.DecimalField()
+    extras = models.ManyToManyField(Sub)
+
+
+class Pasta(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.DecimalField()
+
+
+class Salad(models.Model):
+    name = models.CharField(max_length=64)
+    price = models.DecimalField()
+
+
+class DinnerPlatter(models.Model):
+    name = models.CharField(max_length=64)
+    small_price = models.DecimalField()
+    large_price = models.DecimalField()
