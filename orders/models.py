11f397da8f91c0fa377ad96e4dc5546808626ab2
a-fayez <ahmedf.higgy@gmail.com>
2020-06-06 09:53:25 +0200

    better model string representation

--- orders/models.py
+++ orders/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here.  
 class Topping(models.Model):
     name = models.CharField(max_length=64)
     price = models.DecimalField(blank=True)
+
+    def __str__(self):
+        print(f"{self.name}: {self.price}")
 
 
 # add-ons on the subs
@@ -12,12 +15,18 @@ class Extra(models.Model):
     small_price = models.DecimalField()
     large_price = models.DecimalField()
 
+    def __str__(self):
+        print(f"{self.name} small: {self.small_price}, large: {self.large_price}")
+
 
 class RegularPizza(models.Model):
     name = models.CharField(max_length=64)
     small_price = models.DecimalField()
     large_price = models.DecimalField()
     toppings = models.ManyToManyField(Topping)
+
+    def __str__(self):
+        print(f"{self.name}, small: {self.small_price}, large: {self.large_price}")
 
 
 class SicilianPizza(models.Model):
@@ -26,6 +35,9 @@ class SicilianPizza(models.Model):
     large_price = models.DecimalField()
     toppings = models.ManyToManyField(Topping)
 
+    def __str__(self):
+        print(f"{self.name}, small: {self.small_price}, large: {self.large_price}")
+
 
 class Sub(models.Model):
     name = models.CharField(max_length=64)
@@ -33,18 +45,30 @@ class Sub(models.Model):
     large_price = models.DecimalField()
     extras = models.ManyToManyField(Sub)
 
+    def __str__(self):
+        print(f"{self.name} small: {self.small_price}, large: {self.large_price}")
+
 
 class Pasta(models.Model):
     name = models.CharField(max_length=64)
     price = models.DecimalField()
+
+    def __str__(self):
+        print(f"{self.name}: {self.price}")
 
 
 class Salad(models.Model):
     name = models.CharField(max_length=64)
     price = models.DecimalField()
 
+    def __str__(self):
+        print(f"{self.name}: {self.price}")
+
 
 class DinnerPlatter(models.Model):
     name = models.CharField(max_length=64)
     small_price = models.DecimalField()
     large_price = models.DecimalField()
+
+     def __str__(self):
+        print(f"{self.name} small: {self.small_price}, large: {self.large_price}")
