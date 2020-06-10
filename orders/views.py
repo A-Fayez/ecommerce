a8ee1a3c3035b3780da89fd710b8404d55af7678
a-fayez <ahmedf.higgy@gmail.com>
2020-06-10 08:21:01 +0200

    menu context and view

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def index(request):
@@ -15,4 +16,25 @@ def register(request):
 
 
 def menu(request):
-    return render(request, "pizza/menu.html")
+    context = {
+        "regulars": MenuItem.objects.filter(
+            category_id=Category.objects.get(name="Regular Pizza").pk
+        ),
+        "sicilians": MenuItem.objects.filter(
+            category_id=Category.objects.get(name="Sicilian Pizza").pk
+        ),
+        "toppings": Topping.objects.all(),
+        "subs": MenuItem.objects.filter(
+            category_id=Category.objects.get(name="Subs").pk
+        ),
+        "pastas": MenuItem.objects.filter(
+            category_id=Category.objects.get(name="Pasta").pk
+        ),
+        "salads": MenuItem.objects.filter(
+            category_id=Category.objects.get(name="Salads").pk
+        ),
+        "platters": MenuItem.objects.filter(
+            category_id=Category.objects.get(name="Dinner Platters").pk
+        ),
+    }
+    return render(request, "pizza/menu.html", context)
